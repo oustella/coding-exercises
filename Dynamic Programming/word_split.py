fd3d9eeb9e96af75221ef9e44e1de6964b09ffaf
oustella <oustella@gmail.com>
2021-04-01 23:47:55 -0400

    new problems

--- Dynamic Programming/word_split.py
+++ Dynamic Programming/word_split.py
@@ -11,27 +11,27 @@
 # if it reaches the end when s becomes empty, then it is an accpetable combination.
 
 
-def wordSplit(s, vocab):
-    results = []
-    results.append(back_track(s, vocab))
-    return results
+# def wordSplit(s, vocab):
+#     results = []
+#     results.append(back_track(s, vocab))
+#     return results
 
 # current_solution = []
-def back_track(s, vocab):
-    current_solution = []
-    if not s: 
-        return current_solution
-    for i in range(1, len(s)+1):
-        if s[:i] in vocab:
-            current_solution.append(s[:i])
-            current_solution.extend(back_track(s[i:], vocab))
-    return current_solution
-back_track(s,vocab)
+def wordSplit(s, vocab):
+    def dfs(s, path, res):
+        if not s: 
+            res.append(path[:])
+            return 
+        for i in range(1, len(s)+1):
+            if s[:i] in vocab:
+                path.append(s[:i])
+                dfs(s[i:], path, res)
+                path.pop()
+    res = []
+    dfs(s, [], res)
+    return res
 
-
-current_solution
-wordSplit(s, vocab)
 if __name__=="__main__":
     s = "abcde"
-    vocab = ["ab", "cd", "e", "cde"]
+    vocab = ["a","ab", "cd", "e", "cde"]
     wordSplit(s, vocab)
