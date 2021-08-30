1101364cd18651e45fb36adea6262f452cf8d0a8
oustella <oustella@gmail.com>
2021-08-30 11:22:00 -0400

    small things

---
+++ Arrays and Strings/misc.py
@@ -0,0 +1,7 @@
+# string.split() splits a word into letters, and a sentence into words.
+s = "Hello world"
+print(s.split())
+
+# string.isalum() tests whether all chars in the text are alphanumeric
+s = "?"
+print(s.isalnum())
