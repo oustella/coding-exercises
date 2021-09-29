5cf440df47b6fe6cab1a4ce6b522e67e5d5c0429
oustella <oustella@gmail.com>
2021-09-29 11:45:23 -0400

    update

--- LC994 Rotting Oranges.py
+++ LC994 Rotting Oranges.py
@@ -1,7 +1,18 @@
 from collections import deque
 from typing import List
 
-# Solution 2 for optimal time and space
+# You are given an m x n grid where each cell can have one of three values:
+
+#     0 representing an empty cell,
+#     1 representing a fresh orange, or
+#     2 representing a rotten orange.
+
+# Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+
+# Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
+
+# Key insight: use BFS for minimal time
+# Jump to Solution 2 for optimal time and space
 
 class Solution:
     # Solution 1
