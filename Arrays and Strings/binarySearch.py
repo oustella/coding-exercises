32553473286a20404a55a9d853e4a2ed29691055
oustella <oustella@gmail.com>
2021-12-08 13:43:33 -0500

    clean up. added simplified approach.

--- Arrays and Strings/binarySearch.py
+++ Arrays and Strings/binarySearch.py
@@ -24,7 +24,7 @@ from typing import List
 # 2. li = 0, ri = mid-1 = -1, li>ri, return -1
 
 
-def binarySearch(arr, li, ri, t):
+def binary_search(arr, li, ri, t):
     # as long as the range between li and ri is an effective one
     # when li==ri, it's essentially one element
     if li <= ri:
@@ -33,17 +33,17 @@ def binarySearch(arr, li, ri, t):
         if t == arr[mid]:
             return mid
         elif t < arr[mid]:
-            return binarySearch(arr, li, mid-1, t)  # mid has to move so it doesn't get stuck with one element at either end
+            return binary_search(arr, li, mid-1, t)  # mid has to move so it doesn't get stuck with one element at either end
         else:
-            return binarySearch(arr, mid+1, ri, t)
+            return binary_search(arr, mid+1, ri, t)
     else:
         return -1
 
 test = [1,2]
-binarySearch(test, 0, len(test)-1, 1)
+binary_search(test, 0, len(test)-1, 1)
 
-############ simiplified ##########
-def binary_search(arr: List[int], target: int) -> int:
+############ slightly simiplified ##########
+def binary_search1(arr: List[int], target: int) -> int:
     def helper(l, r):
         if l > r:
             return -1
@@ -57,13 +57,17 @@ def binary_search(arr: List[int], target: int) -> int:
         return helper(l,r)
     return helper(0, len(arr)-1)  # 1
 
-binary_search([1,2,3],4)
+binary_search1([1,2,3],4)
 
 # 1, the initial rindex is the rightmost an index can go
 # it needs to be the last VALID index
 
 ############ simiplified, without recursion ##########
-def binary_search(arr: List[int], target: int) -> int:
+# Note this approach only returns the index of the input array. 
+# If binary search is used as part of an algorithm that needs to search different parts of a mother array
+# this approach would not output the correct index of the mother array
+# See example below for when binary search a rotated array
+def binary_search2(arr: List[int], target: int) -> int:
     l = 0
     r = len(arr)-1
     while l <= r:
@@ -75,26 +79,18 @@ def binary_search(arr: List[int], target: int) -> int:
         else:
             r = mid - 1
     return -1
-    
+binary_search2([1,2,3,4],4)
 
-binary_search([1,2,3,4],4)
 
 
 ###################################
 # find element in a sorted but rotated array
 # e.g. given arr={5, 6, 7, 8, 9, 10, 1, 2, 3}, target=9; return 4
 
-def pivotBinarySearch(arr, n, t):
-    pivot = findPivot(arr, 0, n-1)
-
-    if arr[pivot] == t:
-        return pivot
-    if arr[0] <= t:
-        return binarySearch(arr, 0, pivot-1, t)
-    return binarySearch(arr, pivot+1, n-1, t)
-
 # pivot is i where arr[i]>arr[i+1]
-def findPivot(arr, start, end):
+# This approach is not recommended due to multiple scenarios
+# Use the alternative approach find_pivot1
+def find_pivot(arr, start, end):
     '''
     start and end are the indices of the two ends of the subarray.
     '''
@@ -120,20 +116,23 @@ def findPivot(arr, start, end):
         return mid-1
     # when pivot is to the right of mid, everything on the left is smaller than pivot
     if arr[start] < arr[mid]:
-        return findPivot(arr, mid+1, end)
+        return find_pivot(arr, mid+1, end)
     # when pivot is to the left of mid, 
-    return findPivot(arr, start, mid-1)
+    return find_pivot(arr, start, mid-1)
     
 t1 = [1,2,5,6,10,11]
 t2 = [10,11,1,2,5,6]
 # t1 = []
-findPivot(t2,0,len(t2)-1)
-pivotBinarySearch(t1, len(t1),1)
+find_pivot(t2,0,len(t2)-1)
 
 
-####### alt approach #######
 
-def find_min_rotated(arr: List[int]) -> int:
+####### alt approach findPivot #######
+# the key is to realize that the arr can be translated into
+# a series of continuous False and continue True values
+# e.g. [4,1,2,3] -> [False, True, True, True] if we test arr[i] <= arr[-1]
+# Then the problem is to find the first True index
+def find_pivot1(arr: List[int]) -> int:
     l = 0
     r = len(arr)-1
     last = arr[-1]
@@ -147,4 +146,16 @@ def find_min_rotated(arr: List[int]) -> int:
             l = mid+1
     return bound
 
-find_min_rotated(t2)
+def pivot_binary_search(arr, target):
+    pivot = find_pivot1(arr)
+
+    if arr[pivot] == target:
+        return pivot
+    if arr[0] <= target:
+        return binary_search(arr, 0, pivot-1, target)
+    return binary_search(arr, pivot+1, len(arr)-1, target)
+
+
+if __name__ == "__main__":
+    test = [5,6,1,2,3,4]
+    print(pivot_binary_search(test, 2))
